dc20996ba00923b1338e4151ec2fb09b799dbd54
Catalin <catalinborzei@yahoo.com>
2024-09-24 16:35:37 +0300

    smoll changes
    created template.html for subpages and minor detail changes

--- index/views.py
+++ index/views.py
@@ -6,209 +6,209 @@ def index(request):
 
 ## Prader-Wili
 def ghiduri(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def protocoale(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def diagnostic(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def tratament_gh(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def tratament(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def evaluare_initiala(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def laboratoare_diagnostic(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def clinici_ingrijire(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def alerte(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 ## Obezitati Rare
 def patologii(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def tratament_obezitati(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def monitorizare(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def laboratoare_diagnostic_obezitati(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def clinici_ingrijire_obezitati(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 ## Traseul pacientului
 
 def centre_de_ingrijire(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def asociatii_de_pacienti(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def medico_social(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def viata_scolara(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def viata_sociala(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def proiecte_de_viata(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def formare_profesionala(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def hobby(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def servicii_sociale(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def tranzitia_adolescent_adult(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def urgente_si_riscuri_de_decompensare(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def calatorii(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def screening(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def forum_de_discutii(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def cercetare_clinica(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def centre_de_expertiza(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def servici_medicale(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def educatia_familiei_si_a_institutiilor(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def comportament(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 ## Educatia terapeutica
 
 def instrumente_dieta(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def workshop(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def retete_culinare(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def ingrijire(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def aspecte_psihologice(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def activitate_fizica(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def instrumente_practice_de_ingrijire(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def tratamentul_cu_hormon_de_crestere(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def link_uri_utile(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def recomandari(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 ## Echipa Multidisciplinara
 
 def profesionisti_implicati(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def plan_actiune(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def grupuri_lucru(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def plan_intalniri(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def cpms(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def plan_boli_rare(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def parteneri(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 ## Formare si Informare
 
 def evenimente(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def newsletter(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def radio_noro(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def webinar(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def tutoriale(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def podcast(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def video(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def formare_pentru_medici(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def formare_pentru_pacienti(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def actualitati(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def tipuri_de_cercetare(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def cercetare_europa(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 # Tratamentul_GH views
 def protocol_de_tratament(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def informatii_despre_medicament(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def protocoale_terapeutice(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
 
 def monitorizarea_tratamentului(request):
-    return render(request, 'index/index.html')
+    return render(request, 'index/template.html')
